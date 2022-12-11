2edf13ac5610f19d9b4d4be7ac83557d496f5ee8
Fabian Petersen <fabian@webrus.co.za>
2022-12-11 11:55:07 +0200

    origin

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/Nav";
+import Header from "./Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
