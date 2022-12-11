4ec6b5ad64e17e930647660a7bc2255eb89f99b3
Fabian Petersen <fabian@webrus.co.za>
2022-12-11 19:15:31 +0200

    product-section-complete

--- src/components/Products.js
+++ src/components/Products.js
@@ -9,6 +9,7 @@ const Products = () => {
     <Wrapper>
       <section className="products">
         <div className="section-center clearfix">
+          {/* Start Products Info */}
           <article className="products-info">
             <div className="section-title">
               <h3>Check Out</h3>
@@ -22,21 +23,19 @@ const Products = () => {
               Inventory
             </a>
           </article>
+          {/* End Prducts Info */}
+
           <article className="products-inventory clearfix">
             <div className="product">
               <img src={Image1} alt="product 1" className="product-img" />
               <h4 className="product-title">Ginger Peach Tea</h4>
               <h4 className="product-price">R65,99</h4>
             </div>
-          </article>
-          <article className="products-inventory clearfix">
             <div className="product">
               <img src={Image2} alt="product 2" className="product-img" />
               <h4 className="product-title">White Tea</h4>
               <h4 className="product-price">R45,99</h4>
             </div>
-          </article>
-          <article className="products-inventory clearfix">
             <div className="product">
               <img src={Image3} alt="product 3" className="product-img" />
               <h4 className="product-title">Fruit Sangaria</h4>
@@ -50,6 +49,64 @@ const Products = () => {
   );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  .products {
+    background: var(--clr-grey-10);
+  }
+
+  .products article {
+    padding: 2rem 0;
+  }
+
+  .product-text {
+    color: var(--clr-grey-5);
+    max-width: 26rem;
+  }
+
+  .product {
+    margin-bottom: 2rem;
+  }
+
+  .product-img {
+    border-radius: var(--border-rad);
+    margin-bottom: 2rem;
+    height: 17rem;
+    object-fit: cover;
+  }
+
+  .product-price {
+    color: var(--clr-primary);
+  }
+
+  @media screen and (min-width: 768px) {
+    .product {
+      float: left;
+      width: 50%;
+      padding-right: 2rem;
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    .product {
+      width: 33.3%;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    .products-info {
+      float: left;
+      width: 30%;
+    }
+
+    .products-inventory {
+      float: left;
+      width: 70%;
+    }
+    .product {
+      margin-bottom: 0rem;
+      padding: 0 1rem;
+    }
+  }
+`;
 
 export default Products;
